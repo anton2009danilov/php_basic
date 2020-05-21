03df3f644bb997836a30f9c2f72a3a8a5721093e
anton2009danilov <anton2009danilov@yandex.ru>
2020-05-21 08:20:52 +0300

    Украшательства css

--- views/menu.php
+++ views/menu.php
@@ -1,20 +1,23 @@
-<ul class="nav justify-content-center bg-secondary">
-    <?foreach($params as $item):?>
-        
-        <li class='nav-item'>
-            <a class= 'nav-link active text-light' href = '<?=$item['link']?>'>
-                <?=$item['name']?>
-                <?if($item['cart']):?>
-                    <?if(!$_SESSION['user']):?>
-                        [ <span id="counter"><?=getTotalQuantity(($_SESSION['id']))?></span> ]
-                    <?else:?>
-                        [ <span id="counter"><?=getTotalQuantity()?></span> ]
-                    <?endif;?>
-                <?endif;?>
-            </a>
-        </li>
-        
-            
-    <?endforeach;?>
-     
-</ul>
+<div class="container bg-nav container-round">
+    <ul class="nav  align-content-center">
+        <a href="../../.." class="logo"></a>
+        <? foreach ($params as $item): ?>
+
+            <li class='nav-item'>
+                <a class='nav-link' href='<?= $item['link'] ?>'>
+                    <?= $item['name'] ?>
+                    <? if ($item['cart']): ?>
+                        <? if (!$_SESSION['user']): ?>
+                            [ <span id="counter"><?= getTotalQuantity(($_SESSION['id'])) ?></span> ]
+                        <? else: ?>
+                            [ <span id="counter"><?= getTotalQuantity() ?></span> ]
+                        <? endif; ?>
+                    <? endif; ?>
+                </a>
+            </li>
+
+
+        <? endforeach; ?>
+
+    </ul>
+</div>
