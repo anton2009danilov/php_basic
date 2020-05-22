07d987e1c9be40699be4af811c5a47ef88c082ef
anton2009danilov <anton2009danilov@yandex.ru>
2020-05-22 18:24:01 +0300

    Исправлено: на страничке товара не работают стили панели навигации

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <link rel="shortcut icon" type="image/png" href="/img/logo.png"/>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <title><?=$title?></title>
 </head>
