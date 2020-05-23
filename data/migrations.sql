1f9c64dbdd4173a012faf0acf3cc88476d900740
anton2009danilov <anton2009danilov@yandex.ru>
2020-05-23 20:36:41 +0300

    Создание таблицы учёта лайков users_liked

--- data/migrations.sql
+++ data/migrations.sql
@@ -1,2 +1,13 @@
-<?php
+drop table if exists users_liked;
+create table users_liked
+(
+    user_id int not null,
+    item_id int not null,
+    liked boolean,
 
+    primary key (user_id, item_id),
+    foreign key likes_user_id_fkey (user_id) references users (id),
+    foreign key likes_item_id_fkey (item_id) references gallery (id)
+);
+
+INSERT INTO `users_liked` (`user_id`, `item_id`, `liked`) VALUES ('2', '1', '1'), ('2', '2', '0');
